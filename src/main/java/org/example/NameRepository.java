ac0a89022116d7290fe8008609880fef7b178c9d
darwinmwstafa <darwin.mustafa92@gmail.com>
2021-12-09 11:32:15 +0100

    Result 2

--- src/main/java/org/example/NameRepository.java
+++ src/main/java/org/example/NameRepository.java
@@ -20,8 +20,11 @@ public class NameRepository
         setNames(temp);
 
         String newName[]=findAll();
-
+        System.out.println( find("Darwen AlTaeshi"));
+        System.out.println(add("Samaar ahmed"));
         clear();
+
+
     }
 
     public static int getSize(){
@@ -42,9 +45,34 @@ public class NameRepository
     public static String[] findAll(){
         return names;
     }
+    public static String find(final String fullName){
+        String st=null;
+        for(String s:names){
+
+            if(s.equals(fullName)){ st=fullName;}
+        }
+        return st;
+    }
+
+    public static boolean add(final String fullName){
+        boolean check=true;
+        for(String s:names){
+            if(s.equals(fullName)){ check=false;}
+        }
+        if(check==true){
+            names[0]=fullName;
+        }
+ return  check;
+    }
 }
 
+/*
 
+    public static String find(final String fullName)
+    Returns name if found and null if not found.
 
-
+public static boolean add(final String fullName)
+        Should add a new name to the array. Returns true when name was added and false when the array contains
+        the name.
+ */
 
